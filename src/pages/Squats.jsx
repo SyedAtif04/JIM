cb81f376e36570dd083d90dde5c42e396f4ee7d6
SyedAtif04 <atifsyed1806@gmail.com>
2024-04-29 23:54:49 +0530

    abc

--- src/pages/Squats.jsx
+++ src/pages/Squats.jsx
@@ -18,13 +18,13 @@ export const Squat = () => {
                 <span className="span">To perform Squats </span>
                 <span className="text-wrapper-2"> </span>
                 <span className="text-wrapper-3">
-                  <br/>Setup: Stand in front of cable machine with straight bar attached to high pulley. Grip bar with overhand grip, hands shoulder-width apart, and extend arms downward.
-                  <br/>Positioning: Keep feet shoulder-width apart, knees slightly bent, maintain straight posture with engaged core for stability.
-                  <br/>Execution: Keep upper arms stationary and close to body. Exhale, push bar down by straightening arms until fully extended. Contract triceps throughout.
-                  <br/>Controlled Movement: Inhale as bar returns slowly to starting position, maintaining control. Avoid letting weight stack slam down.
-                  <br/>Repetitions and Sets: Aim for 3-4 sets of 8-12 reps. Adjust weight to fit strength level. Rest 60-90 seconds between sets.
-                  <br/>Form and Safety: Maintain proper form to engage muscles and prevent injury. Consider working with certified trainer for beginners.
-                  <br/>Start Light: Begin with less weight and more reps to aid muscle growth.
+                  <br/>1.Setup: Stand in front of cable machine with straight bar attached to high pulley. Grip bar with overhand grip, hands shoulder-width apart, and extend arms downward.
+                  <br/>2.Positioning: Keep feet shoulder-width apart, knees slightly bent, maintain straight posture with engaged core for stability.
+                  <br/>3.Execution: Keep upper arms stationary and close to body. Exhale, push bar down by straightening arms until fully extended. Contract triceps throughout.
+                  <br/>4.Controlled Movement: Inhale as bar returns slowly to starting position, maintaining control. Avoid letting weight stack slam down.
+                  <br/>5.Repetitions and Sets: Aim for 3-4 sets of 8-12 reps. Adjust weight to fit strength level. Rest 60-90 seconds between sets.
+                  <br/>6.Form and Safety: Maintain proper form to engage muscles and prevent injury. Consider working with certified trainer for beginners.
+                  <br/>7.Start Light: Begin with less weight and more reps to aid muscle growth.
                 </span>
               </p>
             </div>
